550070a4c84f355fb5bb415b0c261e4e6472ae0e
Olesia Sikora <sikora0310@gmail.com>
2020-02-05 12:36:32 +0200

    HW2

---
+++ HomeWork2/homeWork2.js
@@ -0,0 +1,33 @@
+ function checkNumber(N) {
+     while (+N != N || isNaN(parseInt(N, 10))) {
+         alert("Це не число");
+         N = prompt('Введіть ціле число');
+     }
+     return N;
+ }
+ let startNum = prompt('Введіть перше число', '1');
+ startNum = checkNumber(startNum);
+
+ let endNum = prompt('Введіть друге число', '10');
+ endNum = checkNumber(endNum);
+
+ let missEvenNum = confirm("Парні пропускаємо?");
+ if (missEvenNum == true) {
+     document.writeln("Парні числа пропускаємо ")
+ } else {
+     document.writeln("Парні числа не пропускаємо ");
+ }
+
+ let sum = 0;
+ if (missEvenNum == false) {
+     for (let i = +startNum; i <= +endNum; i++) {
+         sum = sum + i
+     }
+ } else {
+     for (let i = +startNum; i <= +endNum; i++) {
+         if (i % 2 != 0) {
+             sum = sum + i
+         }
+     }
+ }
+ document.write(`Перше число ${startNum}, Друге число ${endNum}. \n Cума ${sum}`);
