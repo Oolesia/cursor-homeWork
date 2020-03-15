38f330cbc412e139453832c5098f228a1137d606
Olesia Sikora <sikora0310@gmail.com>
2020-03-15 19:50:53 +0200

    HW11

--- HomeWork11/main.js
+++ HomeWork11/main.js
@@ -1,23 +1,27 @@
-function chinese(str){
+function getChinese(str) {
     const time = 50;
-    return new Promise((resolve)=>{
-    setTimeout(() => {
-        resolve(String.fromCharCode(str));
-    }, time);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(String.fromCharCode(str));
+        }, time);
     })
 }
-async function getRandomChinese(length){
+async function getRandomChinese(length) {
     const n = 5;
-    let result ='';
-        while(length>0){
-        let str='';
+    let result = '';
+    while (length > 0) {
+        let str = '';
         const sign = Date.now().toString();
         for (let i = sign.length - n; i < sign.length; i++) {
             str += sign[i];
         }
-        result+= await chinese(str);
+        result += await getChinese(str);
         length--;
     }
-    console.log(result);
+    return new Promise((resolve) => {
+        resolve(result);
+    })
 }
-getRandomChinese(6);
+getRandomChinese(4).then((res) => {
+    console.log(res);
+});
